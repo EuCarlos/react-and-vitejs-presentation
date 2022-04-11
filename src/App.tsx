1366eca45a0021e9c75dfaa4ad4e5dc4c42569a4
Carlos Alve <carlosalves369m@hotmail.com>
2022-04-11 20:19:25 -0300

    :art: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import './App.css'
+import products from './database/products.json'
+import { ShoppingCard } from './components/ShoppingCart'
+
 
 function App() {
+  
+  const shopping_cart = products.map(product => {
+      return (
+        <ShoppingCard
+          id={product.id}
+          product_name={product.product_name}
+          price={product.price}
+          stock={product.stock}
+          has_forecast={product.has_forecast}
+          day_for_forecast={product.day_for_forecast}
+          product_limited={product.product_limited}
+        />
+      )
+    })
+  
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Carrinho de compras</h1>
+      {shopping_cart}
+      
+      <footer>
+        <p>Desenvolvido por <a href="https://github.com/EuCarlos/" target="_blank">Carlos Alves</a></p>
+        <p><a href="https://carlosalves.vercel.app/" target="_blank">carlosalves.vercel.app</a></p>
+      </footer>
     </div>
   )
 }
